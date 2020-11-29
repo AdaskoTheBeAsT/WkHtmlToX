89f1c4e69bf57ad050e9a07168e2c853f65e6fe8
Adam Pluciński <adaskothebeast@gmail.com>
2020-11-29 19:10:11 +0100

    upgraded libs added test

--- test/unit/AdaskoTheBeAsT.WkHtmlToX.Test/BasicPdfConverterTest.PdfConverterBase.cs
+++ test/unit/AdaskoTheBeAsT.WkHtmlToX.Test/BasicPdfConverterTest.PdfConverterBase.cs
@@ -616,6 +616,34 @@ namespace AdaskoTheBeAsT.WkHtmlToX.Test
         }
 
         [Fact]
+        public void AddContentStreamShouldNotThrowExceptionWhenProperStreamPassed()
+        {
+            // Arrange
+            var converterPtr = new IntPtr(_fixture.Create<int>());
+            var objectSettingsPtr = new IntPtr(_fixture.Create<int>());
+            const string html = @"<!DOCTYPE html>
+                <html lang=""en"">
+                <head>
+                <meta charset=""utf-8"">
+                <title>title</title>
+                <link rel=""stylesheet"" href=""style.css"">
+                <script src=""script.js""></script>
+                </head>
+                <body>
+                <!-- page content -->
+                </body>
+                </html>";
+            using var memoryStream = new MemoryStream(Encoding.UTF8.GetBytes(html));
+
+#pragma warning disable CS8625 // Cannot convert null literal to non-nullable reference type.
+            Action action = () => _sut.AddContentStream(converterPtr, objectSettingsPtr, memoryStream);
+#pragma warning restore CS8625 // Cannot convert null literal to non-nullable reference type.
+
+            // Act & Assert
+            action.Should().NotThrow();
+        }
+
+        [Fact]
         public void AddContentStreamShouldThrowExceptionWhenTooLargeStreamPassed()
         {
             // Arrange
