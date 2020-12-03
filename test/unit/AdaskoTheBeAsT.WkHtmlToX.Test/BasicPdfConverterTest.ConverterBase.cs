4bbe72bb9c2647215ea5b96406f4b03545512c00
Adam Pluciński <adaskothebeast@gmail.com>
2020-12-03 17:57:19 +0100

    disable warning

--- test/unit/AdaskoTheBeAsT.WkHtmlToX.Test/BasicPdfConverterTest.ConverterBase.cs
+++ test/unit/AdaskoTheBeAsT.WkHtmlToX.Test/BasicPdfConverterTest.ConverterBase.cs
@@ -631,6 +631,7 @@ namespace AdaskoTheBeAsT.WkHtmlToX.Test
             }
             else
             {
+                // ReSharper disable once AccessToDisposedClosure
                 _pdfModule.Verify(
                     m =>
                         m.SetObjectSetting(
